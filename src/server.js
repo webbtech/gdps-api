20f33365f518bfae4c4b249d3027b9d507d76fdb
Ron Dyck <rond@webbtech.net>
2018-09-27 13:55:09 -0400

    updated libs and refactored server environments

--- src/server.js
+++ src/server.js
@@ -1,232 +1,44 @@
-// import { ApolloServer, gql } from 'apollo-server-lambda'
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer} from 'apollo-server-lambda'
+
+import AWS from 'aws-sdk'
 import { createError } from'apollo-errors'
-import { merge } from 'lodash'
 
 import authCheck from './auth/authCheck'
-
-import {
-  typeDef as Dip,
-  resolvers as dipResolvers,
-} from './modules/Dip'
-
-import {
-  typeDef as DipOverShort,
-  resolvers as dipOverShortResolvers,
-} from './modules/DipOverShort'
-
-import {
-  typeDef as DipOSMonthReport,
-  resolvers as dipOSMonthReportResolvers,
-} from './modules/DipOSMonthReport'
-
-import {
-  typeDef as DipOSAnnualReport,
-  resolvers as dipOSAnnualReportResolvers,
-} from './modules/DipOSAnnualReport'
-
-import {
-  typeDef as FuelDeliver,
-  resolvers as fuelDeliverResolvers,
-} from './modules/FuelDeliver'
-
-import {
-  typeDef as FuelDeliveryReport,
-  resolvers as fuelDeliveryReportResolvers,
-} from './modules/FuelDeliveryReport'
-
-import {
-  typeDef as FuelPrice,
-  resolvers as fuelPriceResolvers,
-} from './modules/FuelPrice'
-
-import {
-  typeDef as FuelSale,
-  resolvers as fuelSaleResolvers,
-} from './modules/FuelSale'
-
-import {
-  typeDef as FuelSaleDetailedReport,
-  resolvers as fuelSaleDetailedReportResolvers,
-} from './modules/FuelSaleDetailedReport'
-
-import {
-  typeDef as FuelSaleListReport,
-  resolvers as fuelSaleListReportResolvers,
-} from './modules/FuelSaleListReport'
-
-import {
-  typeDef as ImportData,
-  resolvers as importDataResolvers,
-} from './modules/ImportData'
-
-import {
-  typeDef as PropaneDeliver,
-  resolvers as propaneDeliverResolvers,
-} from './modules/PropaneDeliver'
-
-import {
-  typeDef as PropaneSaleAnnualReport,
-  resolvers as propaneSaleAnnualReportResolvers,
-} from './modules/PropaneSaleAnnualReport'
-
-import {
-  typeDef as PropaneSaleMonthReport,
-  resolvers as propaneSaleMonthReportResolvers,
-} from './modules/PropaneSaleMonthReport'
-
-import {
-  typeDef as PropaneReportDwnld,
-  resolvers as propaneReportDwnldResolvers,
-} from './modules/PropaneReportDwnld'
-
-import {
-  typeDef as Station,
-  resolvers as stationResolvers,
-} from './modules/Station'
-
-import {
-  typeDef as StationTank,
-  resolvers as stationTankResolvers,
-} from './modules/StationTank'
-
-import {
-  typeDef as Tank,
-  resolvers as tankResolvers,
-} from './modules/Tank'
-
-// Setup AWS
-import AWS from 'aws-sdk'
+import graphql from './graphql.js'
 import { config } from './config/dynamo'
-
-AWS.config.update(config)
 
 const AuthorizationError = createError('AuthorizationError', {
   message: 'You are not authorized!',
 })
 
-// Construct a schema, using GraphQL schema language
-/*const Query = gql`
-  type Query {
-    _empty: String
-  }
-`*/
+// Setup AWS
+AWS.config.update(config)
 
-const Query = gql`
-  type Query {
-    hello: String
+// Live server context
+const context = async ({ event }) => {
+  const db = await new AWS.DynamoDB()
+  const docClient = await new AWS.DynamoDB.DocumentClient()
+  let user
+  try {
+    user = await authCheck(event.headers.Authorization)
+  } catch (err) {
+    console.error(err) // eslint-disable-line
+    throw new AuthorizationError()
   }
-`
-
-const Mutation = gql`
-  type Mutation {
-    _empty: String
+  return {
+    db,
+    docClient,
+    user,
   }
-`
-
-// Used primarily as a heartbeat
-const helloResolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
 }
 
-const server = new ApolloServer({
-  typeDefs: [
-    Query,
-    Mutation,
-    Dip,
-    DipOSAnnualReport,
-    DipOSMonthReport,
-    DipOverShort,
-    FuelDeliver,
-    FuelDeliveryReport,
-    FuelPrice,
-    FuelSale,
-    FuelSaleDetailedReport,
-    FuelSaleListReport,
-    ImportData,
-    PropaneDeliver,
-    PropaneReportDwnld,
-    PropaneSaleAnnualReport,
-    PropaneSaleMonthReport,
-    Station,
-    StationTank,
-    Tank,
-  ],
-  resolvers: merge(
-    helloResolvers,
-    dipOSAnnualReportResolvers,
-    dipOSMonthReportResolvers,
-    dipOverShortResolvers,
-    dipResolvers,
-    fuelDeliverResolvers,
-    fuelDeliveryReportResolvers,
-    fuelPriceResolvers,
-    fuelSaleDetailedReportResolvers,
-    fuelSaleListReportResolvers,
-    fuelSaleResolvers,
-    importDataResolvers,
-    propaneDeliverResolvers,
-    propaneReportDwnldResolvers,
-    propaneSaleAnnualReportResolvers,
-    propaneSaleMonthReportResolvers,
-    stationResolvers,
-    stationTankResolvers,
-    tankResolvers
-  ),
+const server = new ApolloServer(graphql)
+server.context = context
 
-  // Live server setup
-  /*context: async ({ event }) => {
-    const db = await new AWS.DynamoDB()
-    const docClient = await new AWS.DynamoDB.DocumentClient()
-    let user
-    try {
-      user = await authCheck(event.headers.Authorization)
-    } catch (err) {
-      console.error(err) // eslint-disable-line
-      throw new AuthorizationError()
-    }
-    return {
-      db,
-      docClient,
-      user,
-    }
-  },*/
-
-  // Local development without authentication
-  context: async () => ({
-    db: await new AWS.DynamoDB(),
-    docClient: await new AWS.DynamoDB.DocumentClient(),
-  }),
-
-  // Local development with authentication headers
-  /*context: async ({ req }) => {
-    let user
-    try {
-      user = await authCheck(req.headers.authorization)
-    } catch (err) {
-      console.error(err) // eslint-disable-line
-      throw new AuthorizationError()
-    }
-    return {
-      db: await new AWS.DynamoDB(),
-      user,
-    }
-  },*/
-})
-
-// console.log('process.env: ', process.env.NODE_ENV)
-
-/*exports.graphqlHandler = server.createHandler({
+exports.graphqlHandler = server.createHandler({
   cors: {
     origin: '*',
-    // methods: '*',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
   },
-})*/
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`) // eslint-disable-line
 })
